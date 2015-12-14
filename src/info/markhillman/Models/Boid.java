d00027524b70d5a07db11791cf0d8539aed621b2
Mark Hillman <markhillman94@googlemail.com>
2015-12-14 17:43:32 +0000

    Corrected Spelling

--- src/info/markhillman/Models/Boid.java
+++ src/info/markhillman/Models/Boid.java
@@ -5,7 +5,7 @@ import org.joml.Vector3f;
 /**
  * Class: Boid
  * Description: This will move the entity according to other
- * boids in the vecinity of this one.
+ * boids in the vicinity of this one.
  * Created by Mark on 13/12/2015.
  */
 public class Boid extends Entity {
