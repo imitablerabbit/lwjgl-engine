d29c543653b2833703c2e1f720106c3bf93fb277
Mark Hillman <markhillman94@googlemail.com>
2015-12-15 15:08:08 +0000

    Reduced the boid flocking algorithms

--- src/info/markhillman/Models/Boid.java
+++ src/info/markhillman/Models/Boid.java
@@ -15,18 +15,15 @@ public class Boid extends Entity {
 
     //Create an array of all the boids created
     public static List<Boid> boids = new ArrayList<>(0);
-    private Vector3f direction;
     private Vector3f velocity;
     private Vector3f acceleration;
-    private float maxForce = 0.0001f;
-    private float maxSpeed = 0.2f;
+    private float maxSpeed = 0.05f;
 
     public Boid(Vector3f position) {
         super(position, new Vector3f(0.2f, 0.6f, 1f), new Vector3f(0));
-        direction = new Vector3f();
-        velocity = new Vector3f((float)((Math.random() * 2) - 1) * maxSpeed,
-                (float)((Math.random() * 2) - 1) * maxSpeed,
-                (float)((Math.random() * 2) - 1) * maxSpeed);
+        velocity = new Vector3f((float)(Math.random() - 0.5),
+                (float)(Math.random() - 0.5),
+                (float)(Math.random() - 0.5));
         acceleration = new Vector3f(0);
         boids.add(this);
     }
@@ -34,19 +31,12 @@ public class Boid extends Entity {
         this(new Vector3f(0, 0, 0));
     }
 
-    private void limitVector(Vector3f vector, Vector3f limit) {
-        if (vector.x > limit.x)
-            vector.x = limit.x;
-        else if (vector.x < -limit.x)
-            vector.x = -limit.x;
-        if (vector.y > limit.y)
-            vector.y = limit.y;
-        else if (vector.y < -limit.y)
-            vector.y = -limit.y;
-        if (vector.z > limit.z)
-            vector.z = limit.z;
-        else if (vector.z < -limit.z)
-            vector.z = -limit.z;
+    //This will allow the boids to flock and update their positions
+    @Override
+    public void run() {
+        flock();
+        update();
+        borders();
     }
 
     //Flock the boids
@@ -57,16 +47,25 @@ public class Boid extends Entity {
         Vector3f coh = cohesion();
         Vector3f ali = align();
 
-        //Weight the vectors
-        sep.mul(1);
-        coh.mul(1.5f);
-        ali.mul(1f);
-
         //Add the vectors to the acceleration
-        // TODO: 15/12/2015 Make sure that the vetors being added are not always positive 
         acceleration.add(sep);
         acceleration.add(coh);
         acceleration.add(ali);
+
+        for (Boid boid : boids) {
+            //Align the boids
+            float distance = getPosition().distance(boid.getPosition());
+            if (distance < 8f && boid != this) {
+                acceleration.sub(getPosition().sub(boid.getPosition(), new Vector3f()));
+
+                //Separation
+                if (distance < 6f) {
+                    acceleration.add(getPosition().sub(boid.getPosition(), new Vector3f()).mul(1.5f));
+                }
+                acceleration.normalize();
+                limitVector(acceleration, new Vector3f(maxSpeed));
+            }
+        }
     }
 
     //Update the boid
@@ -77,105 +76,18 @@ public class Boid extends Entity {
         acceleration.mul(0);
     }
 
-    //Adjust the acceleration in line with the target
-    private void seek(Vector3f target) {
-        acceleration.add(steer(target, false));
-    }
-
-    //Adjust the acceleration when the target is reached
-    private void arrive(Vector3f target) {
-        acceleration.add(steer(target, true));
-    }
-
-    //Steer the boid
-    private Vector3f steer(Vector3f target, boolean slowdown) {
-        Vector3f steer;
-        Vector3f desired = target.sub(getPosition(), new Vector3f());
-        float d = desired.length();
-        if (d > 0) {
-            // Normalize desired
-            desired.normalize();
-            if ((slowdown) && (d < 100.0))
-                desired.mul((float)(maxSpeed * (d/100.0)));
-            else
-                desired.mul(maxSpeed);
-
-            // Steering = Desired minus Velocity
-            steer = desired.sub(velocity, new Vector3f());
-            steer.max(new Vector3f(maxForce));  			// Limit to maximum steering force
-        }
-        else {
-            steer = new Vector3f(0);
-        }
-        return steer;
-    }
-
-    //Keep a desired separation
+    //Steer the boids making flocking behaviour
     private Vector3f separation() {
-        float desiredseparation = 1f;
-        Vector3f steer = new Vector3f(0,0,0);
-        int count = 0;
-        for (int i = 0 ; i < boids.size(); i++) {
-            Boid other = boids.get(i);
-            float d = getPosition().distance(other.getPosition());
-            if ((d > 0) && (d < desiredseparation)) {
-                Vector3f diff = getPosition().sub(other.getPosition(), new Vector3f());
-                diff.normalize();
-                diff.div(d);
-                steer.add(diff);
-                count++;
-            }
-        }
-        if (count > 0) {
-            steer.div((float)count);
-        }
-        if (steer.length() > 0) {
-            steer.normalize();
-            steer.mul(maxSpeed);
-            steer.sub(velocity);
-            limitVector(steer, new Vector3f(maxForce));
-        }
-        return steer;
+        return new Vector3f();
     }
     private Vector3f cohesion() {
-        float neighbordist = 5f;
-        Vector3f sum = new Vector3f(0);
-        int count = 0;
-        for (int i = 0 ; i < boids.size(); i++) {
-            Boid other = (Boid) boids.get(i);
-            float d = getPosition().distance(other.getPosition());
-            if ((d > 0) && (d < neighbordist)) {
-                sum.add(other.getPosition());
-                count++;
-            }
-        }
-        if (count > 0) {
-            sum.div((float)count);
-            return steer(sum,false);  // Steer towards the location
-        }
-        return sum;
+        return new Vector3f();
     }
     private Vector3f align() {
-        float neighbordist = 2f;	// defined neighbourhood
-        Vector3f steer = new Vector3f(0);
-        int count = 0;
-        for (int i = 0 ; i < boids.size(); i++) {
-            Boid other = (Boid) boids.get(i);
-            float d = getPosition().distance(other.getPosition());
-            if ((d > 0) && (d < neighbordist)) {
-                steer.add(other.velocity);
-                count++;
-            } }
-        if (count > 0) { steer.div((float)count);}
-        if (steer.length() > 0) { // Implement Reynolds: Steering = Desired - Velocity
-            steer.normalize();
-            steer.mul(new Vector3f(maxSpeed));
-            steer.sub(velocity);
-            limitVector(steer, new Vector3f(maxForce));
-        }
-        return steer;
+        return new Vector3f();
     }
 
+    //Make sure the boid doesnt leave the area
     private void borders() {
         if (getPosition().x < -50)
             getPosition().x = 50;
@@ -191,17 +103,20 @@ public class Boid extends Entity {
             getPosition().z = -50;
     }
 
-    //This will allow the boids to flock and update their positions
-    @Override
-    public void run() {
-        flock();
-        update();
-        borders();
-    }
-
-    @Override
-    public String toString() {
-        return "Velocity: " + velocity;
+    //Limit the vector size
+    private void limitVector(Vector3f vector, Vector3f limit) {
+        if (vector.x > limit.x)
+            vector.x = limit.x;
+        else if (vector.x < -limit.x)
+            vector.x = -limit.x;
+        if (vector.y > limit.y)
+            vector.y = limit.y;
+        else if (vector.y < -limit.y)
+            vector.y = -limit.y;
+        if (vector.z > limit.z)
+            vector.z = limit.z;
+        else if (vector.z < -limit.z)
+            vector.z = -limit.z;
     }
 
     //Make sure that the model is always the same for a clone
